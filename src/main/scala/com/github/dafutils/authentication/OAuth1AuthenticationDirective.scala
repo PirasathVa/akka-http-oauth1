a6b961a224640daeaf783fe7b82012252937c7b7
EmilDafinov <emil.dafinov@appdirect.com>
2018-11-22 13:18:58 -0500

    Fix bintray publishing

--- src/main/scala/com/github/dafutils/authentication/OAuth1AuthenticationDirective.scala
+++ src/main/scala/com/github/dafutils/authentication/OAuth1AuthenticationDirective.scala
@@ -1,13 +1,25 @@
 package com.github.dafutils.authentication
 
-import akka.actor.ActorSystem
-import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server.Directive1
 import akka.http.scaladsl.server.Directives.{authenticateOrRejectWithChallenge, extractExecutionContext, extractRequest}
 import com.github.dafutils.authentication
+import com.typesafe.scalalogging.StrictLogging
 
 import scala.concurrent.duration._
+
+class UrlResolver extends StrictLogging {
+  def urlUsedToSign(httpRequest: HttpRequest): String = {
+    val protocolFromHeader = httpRequest.headers
+      .find(_.name equalsIgnoreCase "x-forwarded-proto")
+      .map(_.value)
+
+    val schemeUsedToSign = protocolFromHeader.getOrElse(httpRequest.uri.scheme)
+    val urlUsedToSign = httpRequest.uri.copy(scheme = schemeUsedToSign).toString()
+    logger.info(s"Url used to sign incoming request: $urlUsedToSign")
+    urlUsedToSign
+  }
+}
 
 object OAuth1AuthenticationDirective {
   val authorizationTokenGenerator = new AuthorizationTokenGenerator()
@@ -16,9 +28,8 @@ object OAuth1AuthenticationDirective {
   def apply(credentialsSupplier: KnownOAuthCredentialsSupplier,
             authorizationTokenGenerator: AuthorizationTokenGenerator = new AuthorizationTokenGenerator(),
             oauthSignatureParser: OauthSignatureParser = new OauthSignatureParser(),
-            maxTimestampAge: Duration = 30 seconds)(implicit as: ActorSystem): Directive1[authentication.OAuthCredentials] = {
+            maxTimestampAge: Duration = 30 seconds): Directive1[authentication.OAuthCredentials] = {
 
-    implicit lazy val log: LoggingAdapter = Logging(as, this.getClass)
     val authenticationFactory = new OAuthAuthenticatorFactory(
       credentialsSupplier,
       authorizationTokenGenerator,
@@ -26,26 +37,17 @@ object OAuth1AuthenticationDirective {
       maxTimestampAge
     )
 
+    val urlResolver = new UrlResolver
+
     extractExecutionContext flatMap { implicit ec =>
       extractRequest flatMap { httpRequest =>
         authenticateOrRejectWithChallenge(
           authenticationFactory.authenticatorFunction(
             requestHttpMethodName = httpRequest.method.value,
-            requestUrl = urlUsedToSign(httpRequest)
+            requestUrl = urlResolver.urlUsedToSign(httpRequest)
           ) _
         )
       }
     }
-  }
-
-  private def urlUsedToSign(httpRequest: HttpRequest)(implicit log: LoggingAdapter): String = {
-    val protocolFromHeader = httpRequest.headers
-      .find(_.name equalsIgnoreCase "x-forwarded-proto")
-      .map(_.value)
-
-    val schemeUsedToSign = protocolFromHeader.getOrElse(httpRequest.uri.scheme)
-    val urlUsedToSign = httpRequest.uri.copy(scheme = schemeUsedToSign).toString()
-    log.debug(s"Url used to sign incoming request: $urlUsedToSign")
-    urlUsedToSign
   }
 }
